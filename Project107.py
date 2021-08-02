bde524864436871576c834f70405b55d44101712
anonymouse <70026624+Akshitha296@users.noreply.github.com>
2021-08-02 09:23:41 -0400

    Update Project107.py

--- Project107.py
+++ Project107.py
@@ -6,10 +6,8 @@ df = pd.read_csv("data107.csv")
 
 print(df.groupby("level")["attempt"].mean())
 
-fig = go.Figure(go.Scatter(
-    x = df.groupby("level")["attempt"].mean(),
-    y = ['Level 1', 'Level 2', 'Level 3', 'Level 4'],
-    orientation = 'h'
-))
+mean = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean() 
+
+fig = px.scatter(mean, x="student_id", y="level", size="attempt", color="attempt")
 
 fig.show()
